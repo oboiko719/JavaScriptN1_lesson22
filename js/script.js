19115f34bcf80b21bce13af8348116f0836d510a
oboiko719 <88028228+oboiko719@users.noreply.github.com>
2022-12-01 16:45:46 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//коментарии
+"use strict";
+
+console.log('Учим');
+console.log('js');
+console.log('Учим' + ' js');
+
+// Д/з Змінні в JAVASCRIPT. Константи. Клюові слова LET CONST VAR
+let userColorEyes = 'Blue';
+console.log(userColorEyes);
+
+let user = 'Вася';
+let userName;
+
+userName = user;
+console.log(userName);
+
+let hgg;
+console.log(hgg);
